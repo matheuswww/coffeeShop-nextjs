379d084dc8ce286674039d3364a72c304498cc89
virussv <teteus.30.08.07@outlook.com>
2023-09-10 15:38:03 -0300

    sobre and contato page made

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,19 @@
+import styles from './sobre.module.css'
+
+export default function Sobre() {
+  return (
+    <main className={styles.main}>
+      <section className={styles.section}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores
+          commodi amet saepe maiores at mollitia tenetur? Similique, nulla
+          libero aspernatur modi officia iure eos sit exercitationem sint
+          magnam! Fugiat, eveniet?Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Facere earum molestiae suscipit consectetur, aliquid
+          quae labore soluta. Ipsum mollitia repellendus sunt corporis obcaecati
+          consequatur expedita illo, incidunt amet. Provident, aliquam!
+        </p>
+      </section>
+    </main>
+  )
+}
